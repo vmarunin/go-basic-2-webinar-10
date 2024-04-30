e4b87b7945a06fa82cce51ea9b5fc886d2da78ea
Vladimir Marunin <vmarunin@gmail.com>
2024-04-30 12:12:37 +0300

    2

--- cmd/simpleback/main.go
+++ cmd/simpleback/main.go
@@ -10,9 +10,11 @@ func main() {
 	// Hello world, the web server
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		log.Println("Requested path:", req.URL.Path)
 		io.WriteString(w, "Hello, world!\n")
 	}
 
 	http.HandleFunc("/", helloHandler)
+	log.Println("Server started")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
